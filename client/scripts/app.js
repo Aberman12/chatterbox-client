edea2a5bb56d283e2f636ec133989b1e65a829e0
Alex <aberman12@yahoo.com>
2018-08-10 17:48:43 -0700

    sending and receiving messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,13 @@
 
 var app = {
+  username: 'alekz',
+  roomname: 'lobby',
   init: function() {
     this.fetch();
   },
+
   send: function(message) {
+    console.log('heres the message insid esend', message);
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
@@ -11,8 +15,8 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        app.fetch();
-        console.log('chatterbox: Message sent');
+        // app.fetch();
+        console.log('chatterbox: Message sent', data);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -20,6 +24,7 @@ var app = {
       }
     });
   },
+
   fetch: function() {
     console.log('startead');
     var chat = $('.chat');
@@ -33,9 +38,8 @@ var app = {
       success: function (data) {
         console.log(data);
         data.results.forEach(function(item) {
-            app.renderMessage(item);
-          });
-        console.log('chatterbox: Message sent');
+          app.renderMessage(item);
+        });
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -51,7 +55,19 @@ var app = {
 
   renderMessage(message) {
     var chats = $('#chats');
-    chats.append('<div id="message"><h4 onClick="app.addFriend()">' + message.username + '</h4><p>' + message.text + '</p></div>');
+    chats.append('<div id="message"><h4 onClick="app.addFriend()">' + message.username + ': </h4><p>' + message.text + '</p></div>');
+  },
+  createPostMessage() {
+    var inputValue = $('#input');
+    var messager = inputValue.val();
+    var obj = {
+      text: messager,
+      username: app.username,
+      roomname: app.roomname
+    };
+    console.log('heres your input value', messager);
+    app.send(obj);
+    event.preventDefault();
   },
 
   renderRoom(room) {
